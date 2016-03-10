2a22d01ed660fb2e37c3899cd803d6e87772de6d
Endy Muhardin <endy.muhardin@gmail.com>
2016-03-10 19:42:21 +0700

    starter aplikasi web

---
+++ contoh-kode/aplikasi-http-sederhana/src/main/java/com/muhardin/endy/belajar/http/HaloServlet.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.http;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HaloServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res){
+
+	}
+}
