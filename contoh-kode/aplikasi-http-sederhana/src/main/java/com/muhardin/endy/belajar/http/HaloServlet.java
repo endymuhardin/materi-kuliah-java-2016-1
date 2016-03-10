2cad2a27c28ca8fbc20a135ab41c290c459d8bff
Endy Muhardin <endy.muhardin@gmail.com>
2016-03-10 19:55:22 +0700

    anotasi servlet

--- contoh-kode/aplikasi-http-sederhana/src/main/java/com/muhardin/endy/belajar/http/HaloServlet.java
+++ contoh-kode/aplikasi-http-sederhana/src/main/java/com/muhardin/endy/belajar/http/HaloServlet.java
@@ -4,9 +4,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.annotation.WebServlet;
 
+import java.io.PrintWriter;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/halo", loadOnStartup = 1)
 public class HaloServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req, HttpServletResponse res){
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setContentType("text/html");
 
+		PrintWriter output = res.getWriter();
+		output.println("<html>");
+		output.println("<head>");
+		output.println("<title>Halo Servlet</title>");
+		output.println("</head>");
+		output.println("<body>");
+		output.println("<h1>Halo Servlet</h1>");
+		output.println("</body>");
+		output.println("</html>");
 	}
 }
